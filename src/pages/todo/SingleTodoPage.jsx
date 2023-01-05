c4bbb34a65f937364874f551daf34ba2fda61ac5
Strocs <strocsdev@gmail.com>
2023-01-05 19:11:46 -0300

    Add todo page template

--- src/pages/todo/SingleTodoPage.jsx
+++ src/pages/todo/SingleTodoPage.jsx
@@ -1,21 +1,33 @@
 import { useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { formatDate, getTaskByName } from '../../services'
 import { getTaskById } from '../../services/getTaskById'
 import { TagBar } from './components'
-
+import { IoClose } from 'react-icons/io5'
 export const SingleTodoPage = () => {
   const { todos = [] } = useSelector(state => state.todos)
-  const { state } = useLocation()
-  const { todo, notes, done, created, tags, id } = getTaskById(todos, state)
+  const { pathname, state } = useLocation()
+  const navigate = useNavigate()
+  const onNavigate = () => navigate('/')
+
+  const { todo, notes, done, created, tags, id } = !state
+    ? getTaskById(todos, state)
+    : getTaskByName(todos, pathname)
+
+  const createdDate = formatDate(created)
 
   return (
-    <section className='mt-4'>
-      {/* //TODO: Poder cambiar el texto del todo */}
-      <h1 className='text-c-text'>{todo}</h1>
-      {/* //TODO: Mostrar las notas */}
-      {/* //TODO: Mostrar la fecha de creación */}
-      {/* //TODO: Mostrar los tags */}
-      <TagBar id={id} done={done} created={created} tags={tags} />
+    <section className='mt-4 text-c-text w-full max-w-4xl'>
+      <h2 className='text-c-text'>{todo}</h2>
+      <p>Notas: {JSON.stringify(notes)}</p>
+      <p>Created: {createdDate}</p>
+      <div className='flex items-center gap-2'>
+        <p>Tags: </p>
+        <TagBar id={id} done={done} created={created} tags={tags} />
+      </div>
+      <button onClick={onNavigate} className='bg-c-text text-c-bg'>
+        Close <IoClose className='inline' />
+      </button>
     </section>
   )
 }
