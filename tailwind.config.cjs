911f003dda6fcef5ca5008e428f8a67d96f1d55c
Strocs <strocsdev@gmail.com>
2023-01-06 19:39:50 -0300

    Fix accessibility errors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
         'c-bg': 'var(--main-background)',
         'c-box': 'var(--box-background)',
         'c-gray': 'var(--gray)',
+        'c-red': 'var(--red)'
       },
     }
   },
