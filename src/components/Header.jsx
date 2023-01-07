f477d13b0ffd720ed883718af82144fcaec56aa8
Strocs <strocsdev@gmail.com>
2023-01-07 20:12:40 -0300

    Modify UI for improve UX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { ThemeBar, ItemsLeft, Title } from '.'
 
 export function Header () {
   return (
-    <header className='w-full pt-8 max-w-2xl m-auto'>
+    <header className='w-full pt-8 mb-4 max-w-2xl m-auto'>
       <div className='flex justify-between gap-2'>
         <Title />
         <ThemeBar />
