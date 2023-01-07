d61519a13b671ad15447d7e401159d049e1638f5
Strocs <strocsdev@gmail.com>
2023-01-07 00:03:19 -0300

    Fix redirection on task edit

--- src/pages/taskInfoPage/TaskInfoPage.jsx
+++ src/pages/taskInfoPage/TaskInfoPage.jsx
@@ -1,24 +1,27 @@
 import { useSelector } from 'react-redux'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { formatDate, getTaskByName } from '../../services'
-import { getTaskById } from '../../services/getTaskById'
-import { TagBar } from './components'
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 import { IoClose } from 'react-icons/io5/index.esm?'
+import { formatDate, getTaskByName, getTaskById } from '@services'
+import { EditTask, TagBar } from './components'
+
 export const TaskInfoPage = () => {
   const { tasks = [] } = useSelector(state => state.simpleTask)
-  const { pathname, state } = useLocation()
   const navigate = useNavigate()
-  const onNavigate = () => navigate('/')
-
-  const { task, notes, done, created, tags, id } = !state
+  const { pathname, state } = useLocation()
+  const singleTask = !state
     ? getTaskById(tasks, state)
     : getTaskByName(tasks, pathname)
-
+  if (!singleTask) {
+    return <Navigate to='/' />
+  }
+  const { task, notes, done, created, tags, id } = singleTask
   const createdDate = formatDate(created)
 
+  const onNavigate = () => navigate('/')
+
   return (
-    <section className='mt-4 text-c-text w-full max-w-4xl'>
-      <h2 className='text-c-text'>{task}</h2>
+    <section className='mt-4 mx-auto text-c-text w-full max-w-2xl'>
+      <EditTask task={task} id={id} />
       <p>Notas: {JSON.stringify(notes)}</p>
       <p>Created: {createdDate}</p>
       <div className='flex items-center gap-2'>
