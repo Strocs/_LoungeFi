3698021ff987299022d27f8857e9c9b99c5dcf7e
Strocs <strocsdev@gmail.com>
2023-01-07 03:40:00 -0300

    Add padding on small devices

--- src/pages/mainPage/TasksPage.jsx
+++ src/pages/mainPage/TasksPage.jsx
@@ -5,7 +5,7 @@ export function TasksPage () {
   return (
     <main className='m-auto w-full'>
       <Outlet />
-      <section className='flex flex-col my-6 mx-auto w-full max-w-2xl md:gap-4'>
+      <section className='flex flex-col gap-4 my-6 mx-auto w-full max-w-2xl'>
         <FilterList />
         <CreateTask />
         <TasksList />
