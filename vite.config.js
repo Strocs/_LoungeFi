8476e8f70c8f36ba6b725d4d7b1be91414dc7d19
Strocs <strocsdev@gmail.com>
2023-01-08 22:17:33 -0300

    Add new paths and paths to jsconfig

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,12 @@ export default defineConfig({
     alias: {
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
       '@store': path.resolve(__dirname, './src/store'),
       '@scripts': path.resolve(__dirname, './src/scripts'),
       '@router': path.resolve(__dirname, './src/router'),
-      '@components': path.resolve(__dirname, './src/components')
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants')
     }
   }
 })
