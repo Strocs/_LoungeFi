eacfe299a033ab82548f63d587befd21f9ea81ff
Strocs <strocsdev@gmail.com>
2023-01-08 22:19:32 -0300

    Feat add a note to a task

--- src/pages/taskInfoPage/components/CreateNote.jsx
+++ src/pages/taskInfoPage/components/CreateNote.jsx
@@ -1,9 +1,18 @@
-export const CreateNote = () => {
+import { addNote } from '@store'
+import { useDispatch } from 'react-redux'
+
+export const CreateNote = ({ id, note }) => {
+  const dispatch = useDispatch()
+  const onWriteNote = e => {
+    dispatch(addNote({ id, note: e.target.value }))
+  }
+
   return (
-    // {/* TODO: Add leave a note feat */}
     <textarea
       placeholder='Add a note'
-      className=' p-1 text-sm outline-1 outline-dashed outline-c-text bg-c-box placeholder:font-light placeholder:text-sm'
+      className=' p-1 text-sm font-light outline-1 outline-dashed outline-c-text bg-c-box placeholder:font-light placeholder:text-sm'
+      onChange={onWriteNote}
+      value={note}
     />
   )
 }
