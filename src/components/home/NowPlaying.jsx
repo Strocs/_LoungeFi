05450e193d339747a7f55841c2ee29d00b6b8972
Strocs <strocsdev@gmail.com>
2023-06-08 20:22:10 -0400

    Fix current radio animation

--- src/components/home/NowPlaying.jsx
+++ src/components/home/NowPlaying.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useRef } from 'react'
 
 export const NowPlaying = ({ radio, isPlaying, isStopped }) => {
+	const radioActiveSliced = radio.slice(0, 30) + '...'
+
 	const radioWrapperRef = useRef(null)
 	const containerRef = useRef(null)
 
 	const animationLoop = () => {
 		if (!radioWrapperRef || !containerRef) return
 
-		const { width: containerWidth } = containerRef.current.getBoundingClientRect()
-		const radioWrapperWidth = radioWrapperRef.current.scrollWidth
-		const { width: radioWidth } = radioWrapperRef.current.children[0].getBoundingClientRect()
+		const containerWidth = containerRef.current.getBoundingClientRect().width
+		const radioWidth = radioWrapperRef.current.children[0].getBoundingClientRect().width
 
 		// Gap between two elements based on width and container width
 		const gap = containerWidth - radioWidth
 		radioWrapperRef.current.style.gap = `${gap}px`
 
+		const radioWrapperWidth = radioWrapperRef.current.scrollWidth
 		// Initial and final offset for animations
 		const initialOffset = (radioWrapperWidth - containerWidth + gap / 2) * -1
 		const finalOffset = gap / 2
@@ -34,33 +36,24 @@ export const NowPlaying = ({ radio, isPlaying, isStopped }) => {
 		} else {
 			animationLoop().play()
 		}
-	}, [radio])
-
-	useEffect(() => {
-		const loop = animationLoop()
-		if (!isPlaying) {
-			loop.pause()
-		} else {
-			loop.play()
-		}
-	}, [isPlaying])
+	}, [isPlaying, radio])
 
 	return (
 		<div
 			ref={containerRef}
-			className={`overflow-hidden text-white ${isStopped && 'opacity-0'}`}
+			className={`overflow-hidden text-white ${isStopped ? 'opacity-0' : ''}`}
 		>
 			<div
 				ref={radioWrapperRef}
-				className='flex w-full duration-[5s] ease-linear transition-all'
+				className={`flex w-full ${isPlaying ? 'duration-[5s] ease-linear transition-transform' : ''}`}
 			>
 				<p className='text-[0.75rem] font-light flex-shrink-0'>
 					<b className='font-medium'>Now Playing: </b>
-					{radio.slice(0, 30) + '...'}
+					{radioActiveSliced}
 				</p>
 				<p className='text-[0.75rem] font-light flex-shrink-0'>
 					<b className='font-medium'>Now Playing: </b>
-					{radio.slice(0, 30) + '...'}
+					{radioActiveSliced}
 				</p>
 			</div>
 		</div>
