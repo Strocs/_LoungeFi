60ee9bf451261da95bd281497de19d60900c04bb
Strocs <strocsdev@gmail.com>
2023-08-09 23:13:05 -0400

    Add: utils for format dates and time

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,8 +1,9 @@
+import { formatTime } from './formatTime'
+
 export const formatDate = time => {
-  const date = new Date(time)
-  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
-  const month =
-    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-  const year = date.getFullYear()
-  return `${day}/${month}/${year}`
+	const date = new Date(time)
+	const day = formatTime(date.getDate())
+	const month = formatTime(date.getMonth() + 1)
+	const year = date.getFullYear()
+	return `${day}/${month}/${year}`
 }
