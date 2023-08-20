e95205375714846ffc32ddfec0bd7be303d83431
Strocs <strocsdev@gmail.com>
2023-08-20 00:18:40 -0400

    Add[radio]: radio store and component

--- src/store/loFiRadioStore.js
+++ src/store/loFiRadioStore.js
@@ -1,25 +1,23 @@
 import { create } from 'zustand'
 
+// A list of YouTube LoFi Radio Streams
+const DEFAULT_RADIO_LIST = [
+	{
+		url: 'jfKfPfyJRdk',
+		title: 'lofi hip hop radio 📚 - beats to relax/study to',
+	},
+]
+
 export const useLoFiRadioStore = create((set, get) => ({
-	loFiRadioQueue: [
-		{
-			url: 'https://i.ytimg.com/vi/jfKfPfyJRdk/maxresdefault.jpg',
-			title: 'lofi hip hop radio 📚 - beats to relax/study to',
-		},
-		{
-			url: 'https://pbs.twimg.com/media/FttFV_XX0AYCdOR?format=jpg',
-			title: 'Lofi Pedro Pascal (El Mandaloreano y Last of Us lofi)',
-		},
-	], // A list of youtube LoFi Radio Streams
-	activeRadio: { index: 0, url: 'https://i.ytimg.com/vi/jfKfPfyJRdk/maxresdefault.jpg', title: 'lofi hip hop radio 📚 - beats to relax/study to' },
+	activeRadio: { index: 0, ...DEFAULT_RADIO_LIST[0] },
 	isPlaying: false, // Play/pause button is active.
 	isStopped: true, // The user has clicked the stop button. True if it is.
 	volumen: 0.5,
 	setActiveRadio: (radioIndex = 0) => {
-		set(state => ({ activeRadio: { index: radioIndex, ...state.loFiRadioQueue[radioIndex] } }))
+		set((state) => ({ activeRadio: { index: radioIndex, ...state.loFiRadioList[radioIndex] } }))
 	},
 	onPlayOrPause: () => {
-		set(state => ({ isPlaying: !state.isPlaying }))
+		set((state) => ({ isPlaying: !state.isPlaying }))
 		if (get().isStopped) set({ isStopped: false })
 	},
 	onStop: () => {
@@ -27,11 +25,11 @@ export const useLoFiRadioStore = create((set, get) => ({
 		set({ isStopped: true })
 	},
 	playNext: () => {
-		if (get().activeRadio.index >= get().loFiRadioQueue.length - 1) return get().setActiveRadio(0)
+		if (get().activeRadio.index >= get().loFiRadioList.length - 1) return get().setActiveRadio(0)
 		get().setActiveRadio(get().activeRadio.index + 1)
 	},
 	playLast: () => {
-		if (get().activeRadio.index <= 0) return get().setActiveRadio(get().loFiRadioQueue.length - 1)
+		if (get().activeRadio.index <= 0) return get().setActiveRadio(get().loFiRadioList.length - 1)
 		get().setActiveRadio(get().activeRadio.index - 1)
 	},
 }))
