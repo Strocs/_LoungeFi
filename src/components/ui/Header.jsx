973dea9e0a43053ca4d5a6ea5521fd7e8159dbef
Strocs <strocsdev@gmail.com>
2023-09-03 23:59:25 -0300

    styles[background]: Add gradient to background

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -2,7 +2,7 @@ import { Title } from '@components/ui'
 
 export function Header ({ children }) {
   return (
-    <header className='flex items-start justify-between w-full py-4 max-w-2xl'>
+    <header className='flex items-start justify-between w-full py-4 max-w-xl'>
       <Title />
       {children}
     </header>
