923a489d8df562fea49e7d371c03bab2bc302f6b
Strocs <strocsdev@gmail.com>
2023-09-05 20:33:15 -0300

    Add: Github link button

---
+++ src/components/home/GithubButton.jsx
@@ -0,0 +1,13 @@
+import { GithubIcon } from '../icons'
+
+export const GithubButton = () => {
+  return (
+    <a
+      href='https://github.com/Strocs/TaskDone'
+      target='_blank'
+      rel='noreferrer'
+      className='text-white p-1 justify-self-end rounded-full outline outline-1 outline-white hover:bg-white hover:text-dark transition-all duration-150 cursor-pointer'>
+      <GithubIcon size={20} />
+    </a>
+  )
+}
