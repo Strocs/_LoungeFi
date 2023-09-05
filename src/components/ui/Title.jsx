901f93890fb2dbadf7aee00af20c7968549d4af4
Strocs <strocsdev@gmail.com>
2023-09-05 13:16:29 -0300

    Change[Title]: taskdone to loungfi

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -2,7 +2,7 @@ export function Title ({ name = 'Ignacio' }) {
   return (
     <div className='text-white grid gap-1'>
       <h1 className='text-3xl font-extralight leading-none'>
-        task<b className='font-bold'>Done_</b>
+        Lounge<b className='font-bold'>Fi_</b>
       </h1>
       <span className='text-xs font-light text-grey leading-none'>
         Welcome {name}
