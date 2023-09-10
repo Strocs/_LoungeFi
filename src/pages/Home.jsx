fb87330edfec19ea3de2ae715526208ec84b7ad5
Strocs <strocsdev@gmail.com>
2023-09-10 18:00:55 -0300

    Add: window document title to constant

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import {
   TasksPanel,
   AddTask,
   Radio,
-  ProfileButton,
   NowPlaying,
   GithubButton,
-  WelcomeName
+  WelcomeName,
+  Profile
 } from '@components/home'
 import { Header, Footer, Title } from '@components/ui'
 
@@ -28,7 +28,7 @@ export const Home = () => {
         <TaskGroups />
         <TasksPanel />
         <section className='pb-1 px-3 grid w-full grid-cols-[1fr_2fr_1fr] items-center'>
-          <ProfileButton />
+          <Profile />
           <Radio />
           <GithubButton />
         </section>
