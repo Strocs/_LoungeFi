a903af390414bbcd6c84068419f1b1f3ed3bcad1
Strocs <strocsdev@gmail.com>
2023-09-10 17:59:45 -0300

    styles[UI]: change styles on button and input

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -24,23 +24,26 @@ export const Button = ({ size, color, outline, hover, shadow, onClick = () => {}
 }
 
 const button = tv({
-  base: 'rounded-full font-medium relative w-fit h-fit group transition-all duration-150 active:opacity-80',
+  base: 'rounded-full font-medium relative w-fit h-fit group transition-all duration-150 group whitespace-nowrap',
   variants: {
     color: {
       white: 'bg-white text-dark',
+      'text-white': 'bg-transparent text-white',
       blue: 'bg-blue text-white',
       'text-blue': 'bg-white text-blue',
       red: 'bg-red text-white',
       'text-red': 'bg-white text-red',
       green: 'bg-green text-white',
       'text-green': 'bg-white text-green',
-      transparent: 'bg-transparent text-transparent'
+      transparent: 'bg-transparent text-transparent',
+      grey: 'bg-grey text-white',
+      'text-grey': 'bg-white text-grey',
     },
     hover: {
-      white: 'hover:bg-white hover:text-dark',
-      blue: 'hover:bg-blue hover:text-white',
-      red: 'hover:bg-red hover:text-white',
-      green: 'hover:bg-green hover:text-white'
+      white: 'hover:bg-white hover:text-dark hover:outline-white',
+      blue: 'hover:bg-blue hover:text-white hover:outline-blue',
+      red: 'hover:bg-red hover:text-white hover:outline-red',
+      green: 'hover:bg-green hover:text-white hover:outline-green'
     },
     shadow: {
       white: 'shadow shadow-white',
@@ -52,7 +55,8 @@ const button = tv({
       white: 'outline outline-2 outline-white',
       blue: 'outline outline-2 outline-blue',
       red: 'outline outline-2 outline-red',
-      green: 'outline outline-2 outline-green'
+      green: 'outline outline-2 outline-green',
+      grey: 'outline outline-2 outline-grey'
     },
     size: {
       sm: 'px-2 py-[.03125rem] text-xs',
--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -4,7 +4,7 @@ export const Input = ({ name = '', customClass = '', children, reference, ...pro
       name={name}
       id={name}
       ref={reference}
-      className={`h-full w-full placeholder:text-sm text-sm focus:outline-none bg-white shadow shadow-blue rounded-full px-4 py-1 text-dark outline outline-2 outline-blue ${customClass}`}
+      className={`h-full w-full placeholder:text-sm text-sm  bg-white shadow shadow-blue rounded-full px-4 py-1 text-dark outline outline-2 outline-blue ${customClass}`}
       {...props}
     />
   )
