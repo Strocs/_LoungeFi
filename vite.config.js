a69c25bff4295b43357ba830c6351c8bea2d2646
Strocs <strocsdev@gmail.com>
2023-09-17 00:11:25 -0300

    fix: pwa

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,29 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { manifestForPlugin } from './manifestForPlugin.js'
+
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: '_LoungeFi',
+    short_name: '_LoungeFi',
+    description: 'A -very- simple app to keep focus on your daily tasks.',
+    icons: [
+      {
+        src: '/favicon.svg',
+        size: '100x100',
+        type: 'image/svg+xml'
+      }
+    ],
+    theme_color: '#4FB9D0',
+    background_color: '#0F1013',
+    display: 'standalone',
+    scope: '/',
+    start_utl: '/',
+    orientation: 'portrait'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
