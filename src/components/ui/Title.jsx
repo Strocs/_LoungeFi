5c3f8729eb17e3c09f93f52e8e2182e570347efa
Strocs <strocsdev@gmail.com>
2023-09-21 14:58:38 -0300

    style[Title]: change logo style

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -7,7 +7,15 @@ export function Title() {
       animate={{ y: 0 }}
       transition={{ duration: 0.6, delay: 0.2 }}
       className='text-3xl font-extralight leading-none'>
-      _Lounge
+      _L
+      <img
+        src='/logo_title.png'
+        alt='Icon of the website'
+        className='inline-block h-[18px] aspect-square'
+        width={18}
+        height={18}
+      />
+      unge
       <motion.b
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
