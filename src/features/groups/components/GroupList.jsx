17a392491d675cb7fe101166605bb81fffe22253
StrocsDev <strocsdev@gmail.com>
2023-09-27 23:58:12 -0300

    Fix[Group]: fix overflow on groups with horizontal scroll

--- src/features/groups/components/GroupList.jsx
+++ src/features/groups/components/GroupList.jsx
@@ -1,15 +1,41 @@
 import { useTaskStore } from '@context'
 import { GroupItem, CreateGroup } from '@features/groups'
 import { AnimatePresence } from 'framer-motion'
+import { useRef } from 'react'
 
 export const GroupList = () => {
   const taskData = useTaskStore(state => state.taskData)
 
   const [all, ...groupList] = Object.keys(taskData)
 
+  const listRef = useRef(null)
+
+  const handleWheel = e => {
+    listRef.current.scrollLeft += e.deltaY / 5
+
+    if (listRef.current.scrollLeft === 0) {
+      listRef.current.classList.remove('mask-lr')
+      listRef.current.classList.add('mask-l')
+    } else if (
+      listRef.current.scrollLeft ===
+      listRef.current.scrollWidth - listRef.current.clientWidth
+    ) {
+      listRef.current.classList.remove('mask-lr')
+      listRef.current.classList.add('mask-r')
+    } else {
+      listRef.current.classList.remove('mask-l')
+      listRef.current.classList.remove('mask-r')
+      listRef.current.classList.add('mask-lr')
+    }
+  }
+
   return (
-    <section className='flex gap-2 w-full items-center'>
-      <ul className='flex gap-3  w-full pr-1 pl-[10px] py-1'>
+    <section className='grid grid-cols-[1fr_auto] items-center gap-2 w-full relative h-fit'>
+      <ul
+        ref={listRef}
+        onWheel={handleWheel}
+        className='flex gap-3 h-fit pr-1 pl-[10px] py-1 overflow-x-scroll overscroll-x-contain scrollbar-hide'
+      >
         <GroupItem group={all} />
         <AnimatePresence>
           {groupList.map(group => (
