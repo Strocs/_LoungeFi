32d4b8d1abac4dc2bc122fb71d74a0d0d67bf6ae
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-11 12:33:34 -0300

    style[Group]: change move to all button on delete group modal

--- src/features/groups/components/DeleteGroupModal.jsx
+++ src/features/groups/components/DeleteGroupModal.jsx
@@ -11,9 +11,11 @@ export const DeleteGroupModal = ({ group, onConfirm, onModal }) => {
         initial='hidden'
         animate='visible'
         exit='hidden'
-        className='bg-white rounded-2xl text-dark text-sm p-6 grid gap-3 justify-items-center absolute z-50 top-0 bottom-0 left-0 right-0 w-fit h-fit m-auto text-center shadow shadow-blue'>
+        className='bg-white rounded-2xl text-dark text-sm p-6 grid gap-3 justify-items-center absolute z-50 top-0 bottom-0 left-0 right-0 w-fit h-fit m-auto text-center shadow shadow-blue'
+      >
         <p className='max-w-[14rem]'>
-          Do you want to <b className='text-red'>delete</b> all tasks from <b>{group}</b>?
+          Do you want to <b className='text-red'>delete</b> all <b>tasks</b>{' '}
+          from <b>{group}</b>?
         </p>
         <div className='flex gap-4 justify-center'>
           <Button
@@ -21,15 +23,17 @@ export const DeleteGroupModal = ({ group, onConfirm, onModal }) => {
             color='white'
             hover='blue'
             outline='blue'
-            onClick={() => onConfirm(true)}>
-            Move it to <b>All</b>
+            onClick={() => onConfirm(true)}
+          >
+            Move to <b>All</b>
           </Button>
           <Button
             size='md'
             color='text-red'
             hover='red'
             outline='red'
-            onClick={() => onConfirm(false)}>
+            onClick={() => onConfirm(false)}
+          >
             Just delete it
           </Button>
         </div>
@@ -39,7 +43,8 @@ export const DeleteGroupModal = ({ group, onConfirm, onModal }) => {
           hover='red'
           size='round-sm'
           className='absolute -top-4'
-          onClick={() => onModal(false)}>
+          onClick={() => onModal(false)}
+        >
           <CloseIcon />
         </Button>
       </motion.div>
