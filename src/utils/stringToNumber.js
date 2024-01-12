b298a1de2b02037a1a317b289071e40728955d30
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-12 17:00:46 -0300

    add utils to transform user name on number

---
+++ src/utils/stringToNumber.js
@@ -0,0 +1,12 @@
+export function stringToNumber (inputString) {
+  // Simple hash function
+  let hash = 0
+  for (let i = 0; i < inputString.length; i++) {
+    hash = (hash << 5) - hash + inputString.charCodeAt(i)
+  }
+
+  // Normalize to the range [0, 1]
+  const normalizedHash = (hash % 1000) / 1000
+
+  return normalizedHash
+}
