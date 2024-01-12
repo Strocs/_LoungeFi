e36e03cb2b8193c5d29d80a1fdeb546095ae0c0f
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-12 16:58:26 -0300

    format welcome name

--- src/components/WelcomeName.jsx
+++ src/components/WelcomeName.jsx
@@ -1,3 +1,7 @@
 export const WelcomeName = ({ name = 'Guest' }) => {
-  return <span className='block text-xs font-light text-grey leading-none'>Welcome {name}</span>
+  return (
+    <span className='block text-xs font-light text-grey leading-none'>
+      Welcome {name}
+    </span>
+  )
 }
