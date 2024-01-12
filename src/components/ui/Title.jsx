27c9bbd0ede992d8bc0473498133109e0665584f
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-11 21:22:16 -0300

    add big logo version

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,18 +1,22 @@
 import { motion } from 'framer-motion'
 
-export function Title () {
+export function Title ({ isBig }) {
   return (
     <motion.h1
-      initial={{ y: -50 }}
+      initial={{ y: isBig ? -100 : -50 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.6, delay: 0.2 }}
-      className='text-3xl font-extralight leading-none whitespace-nowrap'
+      className={`${
+        isBig ? 'text-7xl font-light mx-auto' : 'text-3xl font-extralight'
+      } leading-none whitespace-nowrap`}
     >
       _L
       <img
         src='/logo_title.png'
         alt='Icon of the website'
-        className='inline-block h-[18px] aspect-square'
+        className={`${
+          isBig ? 'h-10 w-10' : 'h-[18px]'
+        } inline-block aspect-square`}
         width={18}
         height={18}
       />
