c95bc7df6f8e56bb5c32d37790e32720cea65754
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-14 12:38:20 -0300

    add environment variable log

--- src/features/authentication/firebase/config.js
+++ src/features/authentication/firebase/config.js
@@ -4,7 +4,8 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.FIREBASE_CONFIG)
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
+console.log(firebaseConfig)
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig)
