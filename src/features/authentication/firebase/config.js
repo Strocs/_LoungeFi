c4b8ab06bfbfc2eb68abf12a1ed80e715896a4fd
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-14 15:25:53 -0300

    change firestore config to vite env

--- src/features/authentication/firebase/config.js
+++ src/features/authentication/firebase/config.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.FIREBASE_CONFIG)
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig)
