24267def5fc24b69e70048126889f7fb68fcb7b0
Strocs <strocsdev@gmail.com>
2024-10-26 01:54:27 -0300

    add: title and description component

---
+++ src/components/TitleAndDescription.jsx
@@ -0,0 +1,15 @@
+import { Header, Title } from '@components/ui'
+
+export const TitleAndDescription = ({ noAnimation }) => {
+  return (
+    <Header>
+      <div className='mx-auto flex w-fit flex-col gap-10'>
+        <Title isBig={true} noAnimation={noAnimation} />
+        <p className='mx-auto max-w-[22rem] px-4 text-center text-sm font-light sm:text-base'>
+          Listen to <b className='font-bold'>LoFi music</b> and set a <b className='font-bold'>Pomodoro timer</b> to
+          focus on your <b className='font-bold'>Daily Tasks</b>.
+        </p>
+      </div>
+    </Header>
+  )
+}
