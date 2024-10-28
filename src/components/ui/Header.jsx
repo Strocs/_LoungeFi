13e975cdec6e3f41a9607dd0265c23f98e7a9ea8
Strocs <strocsdev@gmail.com>
2024-10-27 22:32:02 -0300

    add: custom classes to header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,3 +1,7 @@
-export function Header({ children }) {
-  return <header className='flex w-full max-w-xl items-start justify-between gap-4 py-4'>{children}</header>
+import { cn } from '@utils/clsxWithTailwindMerge'
+
+export function Header({ children, className }) {
+  return (
+    <header className={cn('flex w-full max-w-xl items-start justify-between gap-4 py-4', className)}>{children}</header>
+  )
 }
