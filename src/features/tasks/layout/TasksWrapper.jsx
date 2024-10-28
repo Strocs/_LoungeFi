8417a62c22bb7adf70ec307d1d0a6378d50a0cec
Strocs <strocsdev@gmail.com>
2024-10-27 22:34:34 -0300

    style: add clsx and tmerge to taskwrapper

--- src/features/tasks/layout/TasksWrapper.jsx
+++ src/features/tasks/layout/TasksWrapper.jsx
@@ -1,4 +1,5 @@
 import { useRadioStore } from '@features/radio/store'
+import { cn } from '@utils/clsxWithTailwindMerge'
 
 // TODO: Add swipe between Tasks Panels changing the Task Group selected
 
@@ -6,7 +7,7 @@ export const TasksWrapper = ({ children }) => {
   const isRadioOn = useRadioStore((state) => state.isRadioOn)
 
   return (
-    <section name='tasks-panel' className={`relative rounded-xl text-slate-100 ${isRadioOn ? 'bg-dark/20' : ''}`}>
+    <section name='tasks-panel' className={cn('relative rounded-xl text-slate-100', isRadioOn && 'bg-dark/20')}>
       {children}
     </section>
   )
